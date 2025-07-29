b9c71e9ec712f215682f55d6d4c62c073cb5e5a1
Luis Guilherme <guilherme32698@gmail.com>
2025-07-29 14:57:28 -0300

    Organização e planejamento do sistema

---
+++ sistema-csharp/sistema-hospital-csharp/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_hospital_csharp
+{
+    public class Pessoa
+    {
+        
+    }
+}
---
+++ sistema-csharp/sistema-hospital-csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace sistema_hospital_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+        
+        }
+    }
+}
